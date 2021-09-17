8c53e95d6ae0f904ea4ef49078b0959b53a5768b
Karen Payne <Karen.1.Payne@oregon.gov>
2021-09-17 08:30:05 -0700

    Singleton

---
+++ PlayGroundConsoleNetCoreApp/Classes/ApplicationSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlayGroundConsoleNetCoreApp.Classes
+{
+    /// <summary>
+    /// Discussion
+    /// </summary>
+    public sealed class ApplicationSettings
+    {
+        private static readonly Lazy<ApplicationSettings> Lazy = new(() 
+            => new ApplicationSettings());
+
+        /// <summary>
+        /// Access point to methods and properties
+        /// </summary>
+        public static ApplicationSettings Instance => Lazy.Value;
+        public char[] ChartArray { get; set; }
+    }
+
+}
